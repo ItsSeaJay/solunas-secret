3ab549038ecf38a19f2f777b388fa33171a34f68
Callum John <b6010572@my.shu.ac.uk>
2017-09-11 13:28:07 +0100

    Started playing with projectors.

---
+++ Soluna's Secret/Assets/_Own/Scripts/Player.cs
@@ -0,0 +1,25 @@
+
+/******************************************************************************
+* Product:     Soluna's Secret
+* Script:      Player
+* Description: N/a
+* Author(s):   Callum John
+* Date:        9/11/2017 9:41:59 AM
+******************************************************************************/
+
+// Libraries
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour
+{ 
+	void Start ()
+	{
+		
+	} // End void Start ()
+
+	void Update ()
+	{
+		
+	} // End void Update ()
+} // End public class Player : MonoBehaviour
