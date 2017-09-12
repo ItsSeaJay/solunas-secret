fd8ac1be825ac38417fc03b465b3e52585e2de2e
Callum John <b6010572@my.shu.ac.uk>
2017-09-12 15:49:30 +0100

    Added in a custom shader for projector lights. Imported the standard effects assets and filed them away. Created a 'hand' with visible mesh for placing held objects on the screen. Created a script for locking the rotation of an object, but it's empty.

---
+++ Soluna's Secret/Assets/_Own/Scripts/LockTransformRotation.cs
@@ -0,0 +1,26 @@
+
+/******************************************************************************
+* Product:     Soluna's Secret
+* Script:      LockTransformRotation
+* Description: N/a
+* Author(s):   Callum John
+* Date:        9/12/2017 3:47:25 PM
+******************************************************************************/
+
+// Libraries
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockTransformRotation : MonoBehaviour
+{
+	void Start ()
+	{
+		
+	} // End void Start ()
+
+	void Update ()
+	{
+		
+	} // End void Update ()
+} // End public class LockTransformRotation : MonoBehaviour
