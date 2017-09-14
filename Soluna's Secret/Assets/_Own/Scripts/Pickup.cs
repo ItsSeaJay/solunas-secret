f7cfd2e6eec7ebe082c9370b7d0aaf7dc5f124c5
Callum John <b6010572@my.shu.ac.uk>
2017-09-14 15:30:24 +0100

    Added a class for pickups.

---
+++ Soluna's Secret/Assets/_Own/Scripts/Pickup.cs
@@ -0,0 +1,26 @@
+
+/******************************************************************************
+* Product:     Soluna's Secret
+* Script:      Pickup
+* Description: N/a
+* Author(s):   Callum John
+* Date:        9/14/2017 3:30:13 PM
+******************************************************************************/
+
+// Libraries
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+	void Start ()
+	{
+		
+	} // End void Start ()
+
+	void Update ()
+	{
+		
+	} // End void Update ()
+} // End public class Pickup : MonoBehaviour
