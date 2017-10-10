d975da228c3fb55968bafc45bd1ceb596adf31d7
LYND-15\Callum <Callum.J.Cummings@gmail.com>
2017-10-10 13:25:45 +0100

    Started working on the doors. Removed the debug log and added the ability to alter the locked message.

--- Soluna's Secret/Assets/_Own/Scripts/Door.cs
+++ Soluna's Secret/Assets/_Own/Scripts/Door.cs
@@ -2,7 +2,7 @@
 /******************************************************************************
 * Product:     Soluna's Secret
 * Script:      Door
-* Description: The class that controls doors. Can be locked.
+* Description: The class that controls doors. These tcn be locked.
 * Author(s):   Callum John
 * Date:        9/21/2017 3:20:25 PM
 ******************************************************************************/
@@ -25,6 +25,8 @@ public class Door : MonoBehaviour
     private bool open = false;
     [SerializeField]
     private float transitionTime = 1.0f;
+    [SerializeField]
+    private string lockedSubtitle = "Locked";
 
     private Animator animator;
     private Interactable interactable;
@@ -38,19 +40,21 @@ public class Door : MonoBehaviour
         if (tag != "Interactable")
         {
             tag = "Interactable";
-        }
+        } // End if (tag == interactable)
 
+        // Fix animation for when the game starts
         if (open)
         {
             // The door is open
             animator.Play("Opened");
-        }
+        } // End if (open)
         else
         {
             // The door is closed
             animator.Play("Closed");
-        }
+        } // End else (open)
 
+        // Get a reference to the interactable and make it a door
         interactable = GetComponent<Interactable>();
         interactable.KindOfInteractable = Interactable.Kind.Door;
     } // End void Start ()
@@ -70,20 +74,20 @@ public class Door : MonoBehaviour
         } // End if (!locked)
         else
         {
-            Player.Instance.Subs.SetSubtitle("Locked.");
+            Player.Instance.Subs.SetSubtitle(lockedSubtitle);
         } // End if (!locked)
     } // End public void Move ()
 
     private void Open ()
     {
-        Debug.Log("Opened door " + name + ' ' + GetInstanceID().ToString());
+        //Debug.Log("Opened door " + name + ' ' + GetInstanceID().ToString());
         animator.CrossFade("Open", transitionTime);
         open = true;
     } // End if private void Open()
 
     private void Close ()
     {
-        Debug.Log("Closed door " + name + ' ' + GetInstanceID().ToString());
+        //Debug.Log("Closed door " + name + ' ' + GetInstanceID().ToString());
         animator.CrossFade("Close", transitionTime);
         open = false;
     } // End if private void Open()
